d5cff54dc03d9de1a511f70d19362bf82862007b
yqy09091 <yeqiyangasd@gmail.com>
2023-02-05 22:32:23 +0800

    add makan beverage html and store2.js

--- JS/store2.js
+++ JS/store2.js
@@ -1,47 +1,52 @@
-
 let carts = document.querySelectorAll(".add-cart");
 
 let products = [
     {
-        name:"Sandwich",
-        tag:"subsandwitch",
-        price:7.0,
-        inCart:0
-    },
-    {
-        name:"Flatbread",
-        tag:"subflatbread",
-        price:7.5,
-        inCart:0
-    },
-    {
-        name:"Wraps",
-        tag:"subwraps",
-        price:7.5,
-        inCart:0
-    },
-    {
-        name:"Salad",
-        tag:"subsalad",
-        price:6.0,
-        inCart:0
-    },
-    {
-        name:"cooky",
-        tag:"subcookies",
-        price:1.5,
-        inCart:0
-    },
-    {
-        name:"drinks",
-        tag:"subdrinks",
-        price:1.5,
-        inCart:0
-    },
-    {
         name:"Coca Cola",
         tag:"makancola",
         price:2.0,
+        inCart:0
+    },
+    {
+        name:"100 PLUS",
+        tag:"makan100plus",
+        price:1.5,
+        inCart:0
+    },
+    {
+        name:"Bandung",
+        tag:"makanbandung",
+        price:1.5,
+        inCart:0
+    },
+    {
+        name:"Ice Chocolate Latte",
+        tag:"makanchocolate latte",
+        price:2.5,
+        inCart:0
+    },
+    {
+        name:"Coffee",
+        tag:"makancoffee",
+        price:2.0,
+        inCart:0
+    },
+    {
+        name:"Ice Lemon Tea",
+        tag:"makanlemontea",
+        price:1.5,
+        inCart:0
+    },
+    {
+        name:"Tea",
+        tag:"makantea",
+        price:1.5,
+        inCart:0
+    },
+    {
+        name:"Water",
+        tag:"makanwater",
+        price:1.0,
         inCart:0
     }
 ];
