57c768a6e6b90e24ffda79cf0f11dbc98b07c8ba
Peter <petr.liauos@gmail.com>
2023-02-06 11:38:49 +0800

    Update frontpage

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,66 +1,42 @@
-var settings = {
+function signin()
+{
+  var settings = 
+  {
     "async": true,
     "crossDomain": true,
     "url": "https://sgfoodhour-959c.restdb.io/rest/member",
     "method": "GET",
-    "headers": {
+    "headers": 
+    {
       "content-type": "application/json",
       "x-apikey": "<your CORS apikey here>",
       "cache-control": "no-cache"
     }
   }
+}
   
   $.ajax(settings).done(function (response) {
     console.log(response);
   });
-  var jsondata = {"field1": "xyz","field2": "abc"};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://sgfoodhour-959c.restdb.io/rest/member",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json",
-    "x-apikey": "<your CORS apikey here>",
-    "cache-control": "no-cache"
-  },
-  "processData": false,
-  "data": JSON.stringify(jsondata)
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-var jsondata = {"field1": "new value","field2": "xxx"};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://sgfoodhour-959c.restdb.io/rest/member/(ObjectID)",
-  "method": "PUT",
-  "headers": {
-    "content-type": "application/json",
-    "x-apikey": "<your CORS apikey here>",
-    "cache-control": "no-cache"
-  },
-  "processData": false,
-  "data": JSON.stringify(jsondata)
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-var settings = {
+function signindata()
+{
+  var settings =
+  {
     "async": true,
     "crossDomain": true,
-    "url": "https://sgfoodhour-959c.restdb.io/rest/member/(ObjectID)",
-    "method": "DELETE",
-    "headers": {
+    "url": "https://sgfoodhour-959c.restdb.io/rest/member",
+    "method": "POST",
+    "headers": 
+    {
       "content-type": "application/json",
       "x-apikey": "<your CORS apikey here>",
       "cache-control": "no-cache"
-    }
+    },
+    "processData": false,
+    "data": JSON.stringify(jsondata)
   }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+}
